d8b6372e9292bdf776da81111b5205004958219b
Tarun <tarunsharmakavipurapu@gmail.com>
2025-04-02 21:27:44 +0530

    update

--- main.go
+++ main.go
@@ -6,41 +6,44 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
 )
+
+func readStream(read io.ReadCloser, wg *sync.WaitGroup) <-chan string {
+	Line := make(chan string)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(Line)
+		buff := make([]byte, 8)
+
+		for {
+			n, err := read.Read(buff)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+			}
+			str := string(buff[:n])
+			parts := strings.Split(str, "\n")
+		}
+
+	}()
+	return Line
+}
 
 func main() {
 	file, err := os.Open("message.txt")
 	if err != nil {
-		fmt.Errorf("Error Opening file %v", err)
-		return
+		fmt.Printf("Error:", err)
 	}
-	defer file.Close()
-	buff := make([]byte, 8)
-	currentLine := ""
-	for {
+	var wg sync.WaitGroup
 
-		n, err := file.Read(buff)
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-		str := string(buff[:n])
-		parts := strings.Split(str, "\n")
+	_ = <-readStream(file, &wg)
+	// for  := range stream {
+	// 	// fmt.Printf(line)
+	// }
 
-		for i := 0; i < len(parts)-1; i++ {
-			currentLine += parts[i]
-			fmt.Printf("read: %s\n", currentLine)
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
-
-	}
+	wg.Wait()
 
 }
